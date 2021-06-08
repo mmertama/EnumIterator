4b58be8c8f8c2b257799f9624d7243c666c34bf9
Markus Mertama <markusmertama@InstaDiitalsMBP.home>
2021-06-09 00:00:44 +0300

    first release

---
+++ enumiterator.h
@@ -0,0 +1,179 @@
+#ifndef ENUMITERATOR_H
+#define ENUMITERATOR_H
+
+#include <iostream>
+#include <string_view>
+#include <functional>
+#include <array>
+#include <optional>
+
+using namespace std::string_view_literals;
+
+class EnumIterator {
+    template <typename ENUM, ENUM VALUE, std::enable_if_t<std::is_enum<ENUM>::value, bool> = true>
+    static constexpr std::string_view string() noexcept {
+        return __PRETTY_FUNCTION__;
+    }
+
+    template<int index, char c, int current, typename S>
+    static constexpr int pos(S f) noexcept {
+        constexpr bool is_same = f()[current] == c;
+        if constexpr (is_same) {
+            if constexpr (index == 0)
+                return current;
+            else
+                return pos<index - 1, c, current + 1, S>(f);
+        } else {
+            return pos<index, c, current + 1, S>(f);
+        }
+    }
+    template <int index, char c, int at, typename ENUM, ENUM VALUE>
+    static constexpr int find() noexcept {
+        constexpr auto f = []()->std::string_view {
+            constexpr auto s = string<ENUM, VALUE>();
+            return s;
+        };
+        return pos<index, c, at, decltype(f)>(f);
+    }
+    template <typename ENUM, ENUM VALUE>
+    static constexpr std::string_view name() noexcept {
+#ifndef __clang__
+        constexpr auto p1 = EnumReflect::find<1, '=', 0, ENUM, VALUE>();
+        constexpr auto p2 = EnumReflect::find<0, ';', p1, ENUM, VALUE>();
+#else
+        constexpr auto p1 = EnumIterator::find<1, '=', 0, ENUM, VALUE>();
+        constexpr auto p2 = EnumIterator::find<0, ']', p1, ENUM, VALUE>();
+        return std::string_view(EnumIterator::string<ENUM, VALUE>().data() + (p1 + 2), p2 - p1 - 2);
+#endif
+    }
+
+public:
+
+    template <typename ENUM,
+            auto RANGE_MIN = 0,
+            auto RANGE_MAX = 63,
+            int SZ = static_cast<int>(RANGE_MAX) - static_cast<int>(RANGE_MIN) + 1>
+    class Values {
+        template <int P>
+        static void apply(std::array<std::string_view, SZ>& array) {
+            if constexpr (P > 0) {
+                constexpr auto pos = P - 1;
+                array[pos] = name<ENUM, static_cast<ENUM>(pos + static_cast<int>(RANGE_MIN))>();
+                apply<P - 1>(array);
+            }
+        }
+        std::array<std::string_view, SZ> values = []() {
+            std::array<std::string_view, SZ> array = {};
+            apply<SZ>(array);
+
+            return array;
+        } ();
+
+     static std::optional<int> isName(const std::string_view& v) {
+        constexpr char ENUM_SEP = ':';
+        const auto p0 = v.find_first_of(ENUM_SEP);
+        return (p0 != std::string_view::npos && p0 < v.size() + 1 && v[p0 + 1] == ENUM_SEP)
+                ? std::make_optional(p0) : std::nullopt;
+    }
+
+    public:
+
+        struct Iterator {
+            using value_type = std::tuple<ENUM, std::string_view>;
+            using iterator_category = std::bidirectional_iterator_tag;
+            using difference_type   = std::ptrdiff_t;
+            using pointer           = value_type;
+            using reference         = value_type;
+
+            reference operator*() const {return std::make_tuple(static_cast<ENUM>(m_index + RANGE_MIN), m_values[m_index]);}
+            pointer operator->() {return operator*();}
+
+            Iterator(const std::array<std::string_view, SZ>& val, int pos) : m_values(val), m_index(pos) {
+                if(m_index < SZ - 1) { //expected to be valid on start
+                    while(!isName(m_values[m_index]) && m_index < SZ) {
+                        ++m_index;
+                    }
+                }
+            }
+
+            Iterator& operator++() {
+                do {
+                    if(m_index >= SZ - 1)
+                        break;
+                    ++m_index;
+                } while(!isName(m_values[m_index]));
+                return *this;
+            }
+
+            Iterator& operator--() {
+                do {
+                    if(m_index <= 0)
+                        break;
+                    --m_index;
+                } while(!isName(m_values[m_index]));
+                return *this;
+            }
+
+            Iterator operator++(int) {
+                auto tmp = *this;
+                ++(*this);
+                return tmp;
+                }
+
+            Iterator operator--(int) {
+                auto tmp = *this;
+                --(*this);
+                return tmp;
+                }
+
+            friend bool operator== (const Iterator& a, const Iterator& b) { return a.m_index == b.m_index; };
+            friend bool operator!= (const Iterator& a, const Iterator& b) { return a.m_index != b.m_index; };
+
+            const std::array<std::string_view, SZ>& m_values;
+            int m_index;
+        };
+
+        /// begin
+        Iterator begin() const {
+            return Iterator(values, 0);
+        }
+
+        /// end
+        Iterator end() const {
+            return Iterator(values, SZ - 1);
+        }
+
+        /// get a name from
+        static std::optional<std::string_view> nameOf(ENUM e) {
+            int index_of = 0;
+            const auto names = EnumIterator::Values<ENUM, RANGE_MIN, RANGE_MAX>().values;
+            for(const auto& v : names) {
+                if(isName(v)) {
+                    if(static_cast<int>(e) == index_of + static_cast<int>(RANGE_MIN))
+                        return names[index_of];
+                }
+                ++index_of;
+            }
+            return std::nullopt;
+        }
+
+        /// get a value from
+        static std::optional<ENUM> valueOf(std::string_view str) {
+           int index_of = 0;
+            const auto names = EnumIterator::Values<ENUM, RANGE_MIN, RANGE_MAX>().values;
+            for(const auto& v : names) {
+                const auto p0 = isName(v);
+                if(p0) {
+                    if(*p0 < v.size() + 2 && v.substr(*p0 + 2) == str)
+                        return static_cast<ENUM>(index_of + static_cast<int>(RANGE_MIN));
+                }
+                ++index_of;
+            }
+            return std::nullopt;
+        }
+    };
+};
+
+
+
+#endif // ENUMITERATOR_H
---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include "enumiterator.h"
+
+enum class States
+        {
+    symptom,
+    healed,
+    zucker = 3
+        };
+
+enum class Genophenes
+{
+    treee = 92,
+    hulien = 21,
+    doomsday = 103
+};
+
+
+enum class F99 : unsigned {
+    bing, bang, bong
+};
+
+int main() {
+
+    for(const auto& [v, s] : EnumIterator::Values<States>{}) {
+        std::cout << static_cast<int>(v) << " --> " << s << std::endl;
+    }
+
+    EnumIterator::Values<States> v{};
+
+
+    for(auto it = std::make_reverse_iterator(v.end()) ; it != std::make_reverse_iterator(v.begin()); ++it) {
+        std::cout << static_cast<int>(std::get<0>(*it)) << " --> " << std::get<1>(*it) << std::endl;
+    }
+
+    for(const auto& [v, s] : EnumIterator::Values<F99>{}) {
+        std::cout << static_cast<int>(v) << " --> " << s << std::endl;
+    }
+
+    for(const auto& [v, s] : EnumIterator::Values<Genophenes, 20, 110>{}) {
+        std::cout << static_cast<int>(v) << " --> " << s << std::endl;
+    }
+
+    const auto prout = [](auto v0, auto n0) {
+        std::cout << "Hello " << (v0 ? std::to_string(static_cast<int>(*v0)) : "N/A") << std::endl;
+        std::cout << "Hello " << (n0? *n0 : "N/A") << std::endl;
+    };
+
+    prout(
+            EnumIterator::Values<States>::valueOf("symptom"),
+            EnumIterator::Values<States>::nameOf(States::symptom));
+
+    prout(
+            EnumIterator::Values<States>::valueOf("healed"),
+            EnumIterator::Values<States>::nameOf(States::healed));
+
+    prout(
+            EnumIterator::Values<States>::valueOf("zucker"),
+            EnumIterator::Values<States>::nameOf(States::zucker));
+
+    prout(
+            EnumIterator::Values<States>::valueOf("pizzaboy"),
+            EnumIterator::Values<States>::nameOf(static_cast<States>(12)));
+
+    prout(
+            EnumIterator::Values<Genophenes, 20, 110>::valueOf("treee"),
+            EnumIterator::Values<Genophenes, 20, 110>::nameOf(Genophenes::treee));
+
+    prout(
+            EnumIterator::Values<Genophenes, Genophenes::hulien, Genophenes::treee>::valueOf("hulien"),
+            EnumIterator::Values<Genophenes, Genophenes::hulien, Genophenes::treee>::nameOf(Genophenes::hulien));
+
+    prout(
+            EnumIterator::Values<Genophenes, Genophenes::hulien, Genophenes::doomsday>::valueOf("doomsday"),
+            EnumIterator::Values<Genophenes, Genophenes::hulien, Genophenes::doomsday>::nameOf(Genophenes::doomsday));
+
+    return 0;
+}
